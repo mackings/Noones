59415f9667509f59fe864cd9878de1cfacb97158
mackingsley <kingsleyudoma2018@gmail.com>
2024-10-06 16:00:22 +0100

    offers fix

--- src/controllers/Rate Config/Offers Config/paxfulOffer.js
+++ src/controllers/Rate Config/Offers Config/paxfulOffer.js
@@ -1,17 +1,19 @@
 const axios = require('axios');
 const querystring = require('querystring');
 
-
+// Define clientId and clientSecret as constants
 const clientId = "2HATIKk9764Vw2u2OQOOe1Q8vh6vEUom3piooipCQQKOsiP5";
 const clientSecret = "v5gaTp66t8HgEfgNYlUTnzgu1To3f6nEwGqpSvRTfsuWewp6";
 
-const getPaxfulToken = async (clientId, clientSecret) => {
+
+const getPaxfulToken = async () => {
+
     const tokenEndpoint = 'https://accounts.paxful.com/oauth2/token';
 
     const response = await axios.post(tokenEndpoint, querystring.stringify({
         grant_type: 'client_credentials',
-        client_id: clientId,
-        client_secret: clientSecret,
+        client_id: clientId,  // Pass the clientId here
+        client_secret: clientSecret,  // Pass the clientSecret here
     }), {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
     });
@@ -22,15 +24,15 @@ const getPaxfulToken = async (clientId, clientSecret) => {
 
 
 exports.getPaxfulOffers = async (req, res) => {
-
+    
     try {
         const token = await getPaxfulToken();
         const response = await axios.post(
             'https://api.paxful.com/paxful/v1/offer/list',
             querystring.stringify({ 
                 active: true,
-                offer_type:"buy"
-             }),
+                offer_type: "buy"
+            }),
             {
                 headers: {
                     'Authorization': `Bearer ${token}`,
